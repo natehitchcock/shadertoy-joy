fd75fe16b4583ece8f8b99e2329ea0c81f5292d9
nathan.hitchcock <tripnull@gmail.com>
2017-09-07 19:24:06 -0700

    got a circle rendering

--- procedural-geometry/main.glsl
+++ procedural-geometry/main.glsl
@@ -1,5 +1,18 @@
+/*
+ * Procedural geometry
+ *  Phase 1: sphere in solid color
+ *  Phase 2: Raytrace render
+*/
+float sdSphere(vec3 p, vec3 c, float r) {
+    vec3 d = p - c;
+    return length(d) < r ? 1. : 0.;
+}
 
-
-int main() {
-
+void main() {
+    vec2 uv = gl_FragCoord.xy / iResolution.xy;
+    gl_FragColor = mix(
+        vec4(0.1, 0.1, 0.1, 0.1),
+        vec4(0.8, 0.1, 0.1, 1.),
+        sdSphere(vec3(uv, .5), vec3(0.5, 0.5, 0.5), 0.25)
+    );
 }
